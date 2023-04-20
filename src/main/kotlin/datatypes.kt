bd5884b940b3f0816af2b0a97f6122423c402dba
Mr-Stormzy <sethmeritei8@gmail.com>
2023-04-20 15:09:16 +0300

    first commit

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+//Int
+//Strings
+//Char
+//Boolean
+//Float
+//Double
+
+    var city:String = "Nairobi" //String
+    var age:Int = 40 //Int
+    var domain:Char = 'T'
+    var status:Boolean = true
+    var temperature:Float = 39.6F
+    var results:Double = 23.54678
+}
---
+++ src/main/kotlin/exerise1.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    println("Enter your name")
+    var name = readln()!!.toString()
+}
